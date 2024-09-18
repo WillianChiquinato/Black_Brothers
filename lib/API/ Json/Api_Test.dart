d740b84812b5becea0f1a8aa1908f00399396d83
Willian Chiquinato <willianchiquinato@hotmail.com>
2024-09-18 00:34:54 -0300

    TerceiroCommit

---
+++ lib/API/ Json/Api_Test.dart
@@ -0,0 +1,4 @@
+class ApiTeste
+{
+  final baseAPI = "CUZIN";
+}
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -10,7 +10,7 @@ import 'package:flutter_test/flutter_test.dart';
 
 import 'package:projetosflutter/main.dart';
 
-void main() {
+void main() async {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(const MyApp());
