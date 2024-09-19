e5d3f8537e65ea6df6dc92a82f8dc7b05663ade6
Willian Chiquinato <willianchiquinato@hotmail.com>
2024-09-19 00:30:29 -0300

    CommitButtons

--- lib/main.dart
+++ lib/main.dart
@@ -11,71 +11,88 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      //Home é o nosso BODY no Flutter.
-      //Stack, empilhar uma em cima da outra.
-      //Um em cima do outro com stack, literalmente em forma de pilha
-      home: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Stack(
-            alignment: AlignmentDirectional.center,
-            children: [
-              Container(
-                color: Colors.orange,
-                height: 100,
-                width: 100,
-              ),
-              Container(
-                color: Colors.redAccent,
-                height: 50,
-                width: 50,
-              )
-            ],
-          ),
-          Stack(
-            alignment: AlignmentDirectional.center,
-            children: [
-              Container(
-                color: Colors.orange,
-                height: 100,
-                width: 100,
-              ),
-              Container(
-                color: Colors.redAccent,
-                height: 50,
-                width: 50,
-              )
-            ]
-          ),
-          Row(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        //Home é o nosso BODY no Flutter.
+        //Stack, empilhar uma em cima da outra.
+        //Um em cima do outro com stack, literalmente em forma de pilha
+        home: Container(
+          //Filtrando a cor com o RGB, modelando com o fromARGB, mas nao da pra pegar a cor, tem que pegar externa
+          color: Color.fromARGB(255, 24, 100, 1),
+          child: Column(
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-              Container(
-                color: Colors.greenAccent,
-                height: 50,
-                width: 50,
+              Stack(
+                alignment: AlignmentDirectional.center,
+                children: [
+                  Container(
+                    color: Colors.orange,
+                    height: 100,
+                    width: 100,
+                  ),
+                  Container(
+                    color: Colors.redAccent,
+                    height: 50,
+                    width: 50,
+                  )
+                ],
+              ),
+              Stack(
+                  alignment: AlignmentDirectional.center,
+                  children: [
+                    Container(
+                      color: Colors.orange,
+                      height: 100,
+                      width: 100,
+                    ),
+                    Container(
+                      color: Colors.redAccent,
+                      height: 50,
+                      width: 50,
+                    )
+                  ]
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Container(
+                    color: Colors.greenAccent,
+                    height: 50,
+                    width: 50,
+                  ),
+                  Container(
+                    color: Colors.pink,
+                    height: 50,
+                    width: 50,
+                  ),
+                  Container(
+                    color: Colors.purple,
+                    height: 50,
+                    width: 50,
+                  )
+                ],
               ),
               Container(
-                color: Colors.pink,
-                height: 50,
-                width: 50,
+                color: Colors.amber,
+                height: 30,
+                width: 300,
+                child: Text('Inscrever-se',
+                  style: TextStyle(color: Colors.black, fontSize: 26),
+                  textAlign: TextAlign.center,),
               ),
-              Container(
-                color: Colors.purple,
-                height: 50,
-                width: 50,
-              )
+              //Para o botao e o Text para adicinar texto e TEXSTYLE para estilizaçao
+              ElevatedButton(onPressed: () {
+                  print('Pressionou');
+                }, child: Text('Aperta ai pf'),
+              ),
             ],
           ),
-        ],
-      )
+        )
 
       // Column ou Row(
       //   //Column mainAxisAlignment = Eixo principal de alinhamento da COLUNA, Vertical
