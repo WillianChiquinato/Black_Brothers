636f6a54832ee211e776d2c3f8610defa9a839fb
WillianChiquinato <willianchiquinato@hotmail.com>
2025-05-08 00:29:57 -0300

    Descobrimento do erro

--- lib/API/models/modelo_usuario.dart
+++ lib/API/models/modelo_usuario.dart
@@ -1,33 +1,36 @@
 //Modelo de USUARIO
 class UsuarioClass {
-  final int id;
+  final int? id;
   final String login;
   final String senha;
   final String fK_Pessoa_ID;
 
-  UsuarioClass({
-    required this.id,
-    required this.login,
-    required this.senha,
-    required this.fK_Pessoa_ID
-  });
+  UsuarioClass(
+      {this.id,
+      required this.login,
+      required this.senha,
+      required this.fK_Pessoa_ID});
 
   factory UsuarioClass.fromJson(Map<String, dynamic> json) {
     return UsuarioClass(
-      id: json['ID_Usuario'] ?? -1,
+      id: json['ID_Usuario'],
       login: json['Login'] ?? '',
       senha: json['Senha'] ?? '',
       fK_Pessoa_ID: json['FK_Pessoa_ID']?.toString() ?? '',
     );
   }
 
-
   Map<String, dynamic> toJson() {
-    return {
-      'ID_Usuario': id,
+    final data = {
       'Login': login,
       'Senha': senha,
       'FK_Pessoa_ID': fK_Pessoa_ID,
     };
+
+    if (id != null) {
+      data['ID_Usuario'] = id as String;
+    }
+
+    return data;
   }
 }
